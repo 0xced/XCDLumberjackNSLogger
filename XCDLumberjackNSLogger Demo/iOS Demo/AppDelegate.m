56c7c6ef9204ea9872c41b59002933af17a3332d
Cédric Luthi <cedric.luthi@gmail.com>
2016-04-28 21:40:48 +0200

    Use the new binding method in the demo app

--- XCDLumberjackNSLogger Demo/iOS Demo/AppDelegate.m
+++ XCDLumberjackNSLogger Demo/iOS Demo/AppDelegate.m
@@ -11,35 +11,15 @@
 
 @synthesize window = _window;
 
-static NSString * const NSLoggerBonjourServiceNameKey = @"NSLoggerBonjourServiceName";
-
 - (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-	NSString *bonjourServiceName = [[NSUserDefaults standardUserDefaults] stringForKey:NSLoggerBonjourServiceNameKey];
-	XCDLumberjackNSLogger *nsLogger = [[XCDLumberjackNSLogger alloc] initWithBonjourServiceName:bonjourServiceName];
-	[DDLog addLogger:nsLogger withLevel:DDLogLevelAll];
+	[XCDLumberjackNSLogger bindToBonjourServiceNameUserDefaultsKey:@"NSLoggerBonjourServiceName" level:DDLogLevelAll tags:nil];
 	
 	DDTTYLogger *ttyLogger = [DDTTYLogger sharedInstance];
 	ttyLogger.colorsEnabled = YES;
 	[DDLog addLogger:ttyLogger withLevel:DDLogLevelWarning];
 	
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userDefaultsDidChange:) name:NSUserDefaultsDidChangeNotification object:nil];
-	
 	return YES;
-}
-
-- (void) userDefaultsDidChange:(NSNotification *)notification
-{
-	XCDLumberjackNSLogger *nsLogger = [[DDLog allLoggers] filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id  _Nonnull evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
-		return [evaluatedObject isKindOfClass:XCDLumberjackNSLogger.class];
-	}]].firstObject;
-	
-	NSString *bonjourServiceName = [notification.object stringForKey:NSLoggerBonjourServiceNameKey];
-	if (![bonjourServiceName isEqualToString:(__bridge NSString *)nsLogger.logger->bonjourServiceName])
-	{
-		[DDLog removeLogger:nsLogger];
-		[DDLog addLogger:[[XCDLumberjackNSLogger alloc] initWithBonjourServiceName:bonjourServiceName] withLevel:DDLogLevelAll];
-	}
 }
 
 @end
