d3d2945ccf59fbae615b204298fd641f65de82fa
Cédric Luthi <cedric.luthi@gmail.com>
2016-05-27 18:06:23 +0200

    Log the __TEXT,__text section for the verbose level

--- XCDLumberjackNSLogger Demo/iOS Demo/MainViewController.m
+++ XCDLumberjackNSLogger Demo/iOS Demo/MainViewController.m
@@ -6,6 +6,8 @@
 #import "MainViewController.h"
 
 #import <CocoaLumberjack/CocoaLumberjack.h>
+#import <mach-o/ldsyms.h>
+#import <mach-o/getsect.h>
 
 @implementation MainViewController
 
@@ -18,13 +20,15 @@
 
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+	unsigned long length;
+	uint8_t *bytes = getsectiondata(&_mh_execute_header, "__TEXT", "__text", &length);
 	switch (indexPath.row)
 	{
 		case 0: DDLogError(@"Error log sample"); break;
 		case 1: DDLogWarn(@"Warning log sample"); break;
 		case 2: DDLogInfo(@"Info log sample"); break;
 		case 3: DDLogDebug(@"Debug log sample"); break;
-		case 4: DDLogVerbose(@"Verbose log sample"); break;
+		case 4: DDLogVerbose(@"%@", [NSData dataWithBytesNoCopy:bytes length:length freeWhenDone:NO].description); break;
 		default: break;
 	}
 	[tableView deselectRowAtIndexPath:indexPath animated:YES];
